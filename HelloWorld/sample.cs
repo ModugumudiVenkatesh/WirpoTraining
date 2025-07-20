c8d5b1de93d90adcbafb1cbbf87bbf192729d576
Venkatesh <venkateshvenkym3210@gmail.com>
2025-07-20 22:20:06 +0530

    hello world updated

--- HelloWorld/sample.cs
+++ HelloWorld/sample.cs
@@ -1,17 +1,18 @@
 class Start
 {
 
-static void Main()
-{
+    static void Main()
+    {
 
-    Console.WriteLine(" Entry Point of your program");
+        Console.WriteLine(" Entry Point of your program");
         //All objects are stored in heap memory
-    //All variables like local or reference are stored in stack
-    Employee e = new Employee();
+        //All variables like local or reference are stored in stack
+        Employee e = new Employee();
         Console.WriteLine(e.GetType());
         Console.WriteLine(e.GetHashCode());
-    e.Input();
-    e.Display();
+        e.Input();
+        e.Display();
+        Console.WriteLine("hello");
 
 }
 
